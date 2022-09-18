43cb65eeb67836027eaf4cca918bea9daa01ffa4
Maria Virginia <mariav53@gmail.com>
2022-09-18 16:55:13 +0200

    Update name

--- react-guess-the-word-context/src/App.js
+++ react-guess-the-word-context/src/App.js
@@ -20,65 +20,64 @@ import guessedWordsContext from './contexts/guessedWordsContext';
 // example action
 // { type: 'setSecretWord', payload: 'party' }
 const reducer = (state, action) => {
-  switch(action.type) {
-    case 'setSecretWord':
-      return { ...state, secretWord: action.payload }
-    case 'setLanguage':
-      return { ... state, language: action.payload }
-    default:
-      throw new Error(`Invalid action type: ${action.type}`)
-  }
-}
-
+	switch (action.type) {
+		case 'setSecretWord':
+			return { ...state, secretWord: action.payload };
+		case 'setLanguage':
+			return { ...state, language: action.payload };
+		default:
+			throw new Error(`Invalid action type: ${action.type}`);
+	}
+};
 
 function App() {
-  const [state, dispatch] = React.useReducer(
-    reducer,
-    { secretWord: null, language: 'en' }
-  )
+	const [state, dispatch] = React.useReducer(reducer, {
+		secretWord: null,
+		language: 'en',
+	});
 
-  // TODO: get props from shared state
-  const success = false;
-  const guessedWords = [];
+	// TODO: get props from shared state
+	const success = false;
+	const guessedWords = [];
 
-  const setSecretWord = (secretWord) => {
-    dispatch({ type: 'setSecretWord', payload: secretWord });
-  }
-  const setLanguage = (language) => {
-    dispatch({ type: 'setLanguage', payload: language });
-  }
+	const setSecretWord = (secretWord) => {
+		dispatch({ type: 'setSecretWord', payload: secretWord });
+	};
+	const setLanguage = (language) => {
+		dispatch({ type: 'setLanguage', payload: language });
+	};
 
-  useEffect(() => {
-    getSecretWord(setSecretWord);
-  }, [])
+	useEffect(() => {
+		getSecretWord(setSecretWord);
+	}, []);
 
-  if(state.secretWord === null) {
-    return (
-      <div className="container" data-test="spinner">
-        <div className="spinner-border" role="status">
-          <span className="sr-only">Loading...</span>
-        </div>
-        <p>Loading secret word...</p>
-      </div>
-    )
-  }
+	if (state.secretWord === null) {
+		return (
+			<div className='container' data-test='spinner'>
+				<div className='spinner-border' role='status'>
+					<span className='sr-only'>Loading...</span>
+				</div>
+				<p>Loading secret word...</p>
+			</div>
+		);
+	}
 
-  return (
-    <div data-test="component-app" className="container">
-      <h1>Jotto</h1>
-      <p>The secret word is {state.secretWord}</p>
-      <languageContext.Provider value={state.language}>
-        <LanguagePicker setLanguage={setLanguage} />
-        <guessedWordsContext.GuessedWordsProvider>
-          <successContext.SuccessProvider>
-            <Congrats />
-            <Input secretWord={state.secretWord} />
-          </successContext.SuccessProvider>
-          <GuessedWords />
-        </guessedWordsContext.GuessedWordsProvider>
-      </languageContext.Provider>
-    </div>
-  );
+	return (
+		<div data-test='component-app' className='container'>
+			<h1>Guess the word</h1>
+			<p>The secret word is {state.secretWord}</p>
+			<languageContext.Provider value={state.language}>
+				<LanguagePicker setLanguage={setLanguage} />
+				<guessedWordsContext.GuessedWordsProvider>
+					<successContext.SuccessProvider>
+						<Congrats />
+						<Input secretWord={state.secretWord} />
+					</successContext.SuccessProvider>
+					<GuessedWords />
+				</guessedWordsContext.GuessedWordsProvider>
+			</languageContext.Provider>
+		</div>
+	);
 }
 
 export default App;
